8606c2e9a5430824c680df5df8ebf73d887c2dd6
evasalgado <imviarsha@gmail.com>
2024-03-15 16:21:07 -0600

    agregar concierto hecho sin probar

--- Lab9P3_EvaSalgado.cpp
+++ Lab9P3_EvaSalgado.cpp
@@ -2,6 +2,25 @@
 #include "Concierto.h"
 #include <iostream>
 using namespace std;
+GestorVentas gv;
+void agregar_concierto() {
+	string nb, fc;
+	double pe;
+	int code;
+	cout << "Ingrese el nombre de la banda: "<<endl;
+	cin.ignore();
+	getline(cin,nb);
+	cout << "Ingrese el precio de la entrada: "<<endl;
+	cin >> pe;
+	cout << "Ingrese la fecha del concierto(dd/mm/aaaa)" << endl;
+	cin.ignore();
+	getline(cin,fc);
+	cout << "Ingrese el codigo: " << endl;
+	cin >> code;
+	Concierto* c = new Concierto(nb,pe,fc,0.0,0);
+	gv.agregarConcierto(c);
+	cout << "concierto agregado correctamente" << endl;
+}
 int main(){ //inicio de programa
 	int op = 0;
 	do{
@@ -15,6 +34,7 @@ int main(){ //inicio de programa
 		cin >> op;
 		switch (op) {
 		case 1: //ejercicio 1
+			agregar_concierto();
 			break;
 		case 2://ejercicio 2
 			break;
